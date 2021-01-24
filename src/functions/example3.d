f3c9217ac17de4178ebfa52b3705971162b5a79f
sami2020pro <samprogram2007@gmail.com>
2021-01-25 01:03:29 +0330

    function in D language example three

---
+++ src/functions/example3.d
@@ -0,0 +1,13 @@
+import std.stdio;
+
+auto add(int first, double second) {
+	double result = first + second;
+	return result;
+}
+
+void main() {
+	int a = 1;
+	double b = 2.5;
+
+	writeln("add(a, b) = ", add(a, b));
+}
