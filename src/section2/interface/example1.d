84c7e066605c4f0ade931e8ffc940ea8f3bb0b42
sami2020pro <samprogram2007@gmail.com>
2021-01-28 01:49:40 +0330

    interface in D language example one

---
+++ src/section2/interface/example1.d
@@ -0,0 +1,36 @@
+import std.stdio;
+
+// Base class
+interface Shape {
+	public:
+		void setWidth(int w);
+		void setHeight(int h);
+}
+
+// Derived class
+class Rectangle : Shape {
+	int width;
+	int height;
+
+	public:
+		void setWidth(int w) {
+			width = w;
+		}
+
+		void setHeight(int h) {
+			height = h;
+		}
+
+		int getArea() {
+			return (width * height);
+		}
+}
+
+void main() {
+	Rectangle Rect = new Rectangle();
+	Rect.setWidth(5);
+	Rect.setHeight(7);
+
+	// Print the area of the object
+	writeln("Total area: ", Rect.getArea());
+}
